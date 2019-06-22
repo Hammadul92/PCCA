6c45a1c16bc156fd99a06509341f529e72937b58
cdm <sftahir@sfu.ca>
2019-06-21 20:05:01 -0700

    functional login

--- pcca/src/Components/Login/Login.js
+++ pcca/src/Components/Login/Login.js
@@ -6,15 +6,22 @@ import {connect} from 'react-redux';
 import * as actionTypes from '../../store/actions';
 import axios from 'axios';
 
-class Login extends React.Component{
-    state = {res: null, user: null}
+ 
 
-	componentWillMount(){
-		//CREATEING TOKEN//
-	
-		 //ACCESS A PRIVATE ROUTE
-		 
-		 var test2 = {
+class Login extends React.Component{
+    state = {res: null, user: null, username: null , password: null, message: null}
+
+
+
+
+	loginDataHandler = () => {
+		const data = {
+			email: this.state.username,
+			password: this.state.password
+		};
+		console.log(data);
+
+		var test2 = {
 			"async": true,
 			"crossDomain": true,
 			"url": "http://localhost:5000/userlogin",
@@ -29,28 +36,29 @@ class Login extends React.Component{
 			  "cache-control": "no-cache"
 			},
 			"processData": false,
-			"data": "{\n    \"email\": \"info@pcca.com\",\n    \"password\": \"paklah92\"\n}"
+			"data": data
 	
 		  };
-		   axios(test2)
-		   .then(response => {
-			 //console.log(response.data.access_token);
-			 if (response.data.access_token){
-                this.setState({res: response.data.access_token, user: response.data.user});
-                //()=>this.loggedIn(response.data.access_token);
-				//console.log('Response has token ',response.data.user);
-			 }
-			 else{
-				//console.log('STATE logged in');
-			 }
-			 
-		   })
-		   .catch(error=> {
-			 //console.log(error);
-		   });
-	
-	
+
+		  axios(test2)
+		  .then(response => {
+			//console.log(response.data.access_token);
+			if (response.data.access_token){
+			   this.setState({res: response.data.access_token, user: response.data.user , message: response.data.message});
+			   //()=>this.loggedIn(response.data.access_token);
+			   //console.log('Response has token ',response.data.user);
+			}
+			else{
+				this.setState({ message: response.data.message});
+			}
+			
+		  })
+		  .catch(error=> {
+			//console.log(error);
+		  });
+
 	}
+
 
 
 	render (){
@@ -58,10 +66,27 @@ class Login extends React.Component{
         if (this.state.res){
 			var tok = {token: this.state.res , user: this.state.user }
             this.props.loggedIn(tok);
-        }
+		}
+		else{
+			var msg= (<p>{this.state.message} </p>);
+		}
 		return (
 				<Auxilary>
                         <h1> Login</h1>
+						{msg}
+
+						<div className='LoginForm'>
+							<lable>USERNAME</lable>
+							<input type='email' aria-describedby="emailHelp" value={this.state.username} onChange={(event)=>this.setState({username: event.target.value})}/>
+							<lable>Password</lable>
+							<input type='password' value={this.state.password} onChange={(event)=>this.setState({password: event.target.value})}/>
+							
+							<button  className="btn btn-primary" onClick={this.loginDataHandler}>Login</button>
+						</div>
+
+
+
+
 
 	            </Auxilary>
 			);
