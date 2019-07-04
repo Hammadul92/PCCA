95c47fa240f51b879c5812c87ef15ca062e0140c
Hammad <hammad@naturallysplendid.com>
2019-07-04 12:21:09 -0700

    added styling

--- pcca/src/Components/Cart/Proceed.js
+++ pcca/src/Components/Cart/Proceed.js
@@ -5,9 +5,16 @@ import * as actionTypes from '../../store/actions';
 import CheckoutForm from './Checkout';
 import {Elements, StripeProvider} from 'react-stripe-elements';
 import Auxilary from '../../hoc/Auxilary/Auxilary';
+import './Cart.module.css';
 
 class Proceed extends React.Component{
 
+   state = {
+       user: this.props.state.user,
+       firstname: this.props.state.firstname,
+       lastname: this.props.state.lastname,
+       phone: this.props.state.phone,
+    }
 
  
 	render (){
@@ -26,7 +33,32 @@ class Proceed extends React.Component{
 		}
         return(
 
-               <div className="container">   
+               <div className="container"> 
+
+                 <h1> Confirm Your Account </h1>
+                 <div className="payment-tile">                     
+                      <form className="LoginForm row" onSubmit={(event) => this.updateFormHandler(event)}>
+                          <div className="col-md-6 form-group">
+                              <label> Email </label>
+                              <input type="text" value={this.state.user} onChange={(event)=>this.setState({user: event.target.value})} required/>
+                          </div>
+                          <div className="col-md-6 form-group">
+                              <label> Phone Number </label>
+                              <input type="text" value={this.state.phone} onChange={(event)=>this.setState({phone: event.target.value})} required/>
+                          </div>
+                          <div className="col-md-6 form-group">
+                               <label> First Name </label>
+                               <input type="text" value={this.state.firstname} onChange={(event)=>this.setState({firstname: event.target.value})} required/>
+                          </div>
+                          <div className="col-md-6 form-group">
+                               <label> Last Name </label>
+                               <input type="text" value={this.state.lastname} onChange={(event)=>this.setState({lastname: event.target.value})} required/>
+                          </div>
+                          <div className="col-md-6 col-md-offset-3 form-group">
+                               <button className="btn" type="submit"> Update </button>
+                          </div>
+                      </form> 
+                  </div>
                   <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
                      <div>
                         <Elements>
