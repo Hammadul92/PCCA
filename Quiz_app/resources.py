1ae1f2aa68720aff6fe17a16293ede0a7f513ccb
Hammad <hammad@naturallysplendid.com>
2019-07-16 15:11:51 -0700

    changes

--- Quiz_app/resources.py
+++ Quiz_app/resources.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from .models import Media, User, RevokedTokenModel, Gallery, Events, Blogs, Contact, Sales
+from .models import Media, User, RevokedTokenModel, Gallery, Events, Blogs, Contact, Sales, Pages
 from .email import  send_contact_message, contact_confirmation_email
 from .views import get_month_name, date_now, month_now, year_now
 from sqlalchemy import exc, func, cast, DATE, or_
@@ -7,10 +7,6 @@ from flask import request, session, redirect, url_for, flash, g,json,jsonify,abo
 from . import db
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 import stripe
-
-
-
-
 
 
 
@@ -118,10 +114,6 @@ class SecretResource(Resource):
         }
 
 
-
-
-
-
 class GalleryImages(Resource):
     def get(self):
         gallery = Gallery.query.order_by(Gallery.gallery_ID.desc()).all()
@@ -130,6 +122,13 @@ class GalleryImages(Resource):
             response['images'].append({'gallery_ID': pic.gallery_ID, 'img_url': pic.img_url})      
         return response
 
+class AboutUs(Resource):
+    def get(self):
+        page = Pages.query.filter_by(page_name = 'About Us').first()
+        response = {'page_description': ''}
+        if page:
+           response = {'page_description': page.page_description }  
+        return response
 
 
 class BlogPosts(Resource):
